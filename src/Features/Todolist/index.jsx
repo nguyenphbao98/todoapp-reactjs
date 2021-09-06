95768c269d59b1d66336fb82a4447d4b92d80784
just Bảo <nguyenphbao98@gmail.com>
2021-09-06 16:17:05 +0700

    update UI,Task list and set up state for that list

--- src/Features/Todolist/index.jsx
+++ src/Features/Todolist/index.jsx
@@ -1,30 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import deleteLogo from'../../assets/delete-bin.png';
 
-function index() {
+function TodoList() {
+
+    const [checkedTasks,setCheckedTasks] = useState([
+        {
+            id: '1',
+            content: 'This is my task 1',
+            checked: true,
+        },
+
+        {
+            id: '2',
+            content: 'This is my task 2',
+            checked: false,
+        },
+
+        {
+            id: '3',
+            content: 'This is my task 3',
+            checked: false,
+        }
+    ])
+
+    const handleAddTask = (e) => {
+        e.preventDefault();
+
+        console.log(e.target);
+    }
+
+    const handleCheckedTask = (id) => {
+        
+        // remove
+        // const newCheckedTask = checkedTasks.filter(item => item.id !== id)
+        const index = checkedTasks.findIndex(item => item.id === id)
+        if(index < 0)
+            return;
+
+        const newCheckedTask = [...checkedTasks];
+        newCheckedTask[index].checked = !newCheckedTask[index].checked;
+        setCheckedTasks(newCheckedTask);
+    }
+
+    const handleRemoveTask = (id) => {
+        const newCheckedTask = checkedTasks.filter(item => item.id !== id)
+
+        setCheckedTasks(newCheckedTask);
+    }
+
+    // item {
+    //     key,
+    //     content,
+    //     checked,
+    // }
+
     return (
         <div className="todo">
-            <div className="todo__input">
+            <form className="todo__input" onSubmit={(e) => handleAddTask(e)}>
                 <input type="text" id="task" placeholder=" "/>
                 <label>Task</label>
                 <button>Add Task</button>
-            </div>
+            </form>
 
             <div className="todo__content">
                 <ul className="todo__wrapper">
-                    <li className="todo__item">
-                        <label>
-                            <input type="checkbox" name="item1" />
-                            This is item 1
-                            
-                        </label>
-                        <img src={deleteLogo} alt="delete icon"/>
-                        
-                    </li>
+
+                    {
+                        checkedTasks.length > 0 ?
+                            checkedTasks.map(({id, content, checked}) => (
+                                <li className={ `todo__item ${checked ? "text-through" : ""}`} key={id}>
+                                    <label onClick={() => handleCheckedTask(id)}>
+                                        { content }
+                                        
+                                    </label>
+                                    <img src={deleteLogo} alt="delete icon" onClick={() => handleRemoveTask(id)}/>
+                                    
+                                </li>
+                            ))
+                        : <p>Oops, You need to add more tasks</p>
+                    }
+
+                    
                 </ul>
             </div>
         </div>
     )
 }
 
-export default index
+export default TodoList
